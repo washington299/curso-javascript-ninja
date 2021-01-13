2a9f156c050c2a861b49039e2b9803316c562286
Washington <washingtoncampos9@gmail.com>
2021-01-12 22:04:37 -0300

    challenge-10

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -2,29 +2,46 @@
 Crie uma IIFE que envolva todo esse arquivo (inclusive esse comentário),
 e faça a indentação correta.
 */
+(function() {
 
-/*
-Sem alterar os códigos nos `console.log` abaixo, faça com que o retorno
-deles seja "true", usando os Wrapper Objects como "conversores" nos valores
-das variáveis. Analise o que está sendo impresso no console para saber como
-resolver o problema corretamente.
-*/
-var five = '5';
-console.log( five + ' é número?', typeof five === 'number' );
+  /*
+  Sem alterar os códigos nos `console.log` abaixo, faça com que o retorno
+  deles seja "true", usando os Wrapper Objects como "conversores" nos valores
+  das variáveis. Analise o que está sendo impresso no console para saber como
+  resolver o problema corretamente.
+  */
+ var five = Number('5');
+ console.log( five + ' é número?', typeof five === 'number' );
 
-var concat = 10 + 10;
-console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
+ var concat = String('10' + '10');
+ console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
 
-/*
-Voltando ao exemplo da calculadora, vamos utilizar mais uma abordagem
-funcional, mas dessa vez, separando algumas responsabilidades.
-- Primeiro, crie um objeto chamado `operation` que terá as propriedades:
-'+', '-', '*', '/' e '%'.
-- Cada propriedade vai receber uma função (logo, elas serão métodos), e essa
-função receberá dois parâmetros e retornará a operação referente à sua
-propriedade, usando os valores passados por parâmetro.
-*/
-// ?
+ /*
+ Voltando ao exemplo da calculadora, vamos utilizar mais uma abordagem
+ funcional, mas dessa vez, separando algumas responsabilidades.
+ - Primeiro, crie um objeto chamado `operation` que terá as propriedades:
+ '+', '-', '*', '/' e '%'.
+ - Cada propriedade vai receber uma função (logo, elas serão métodos), e essa
+ função receberá dois parâmetros e retornará a operação referente à sua
+ propriedade, usando os valores passados por parâmetro.
+ */
+var operation = {
+  '+': function(num1, num2) {
+    return num1 + num2;
+  },
+  '-': function(num1, num2) {
+    return num1 - num2;
+  },
+  '*': function(num1, num2) {
+    return num1 * num2;
+  },
+  '/': function(num1, num2) {
+    return num1 / num2;
+  },
+  '%': function(num1, num2) {
+    return num1 % num2;
+  },
+};
 
 /*
 Crie uma função chamada `isOperatorValid`, que receberá um operador por
@@ -36,7 +53,9 @@ parâmetro a ela é válido, ou seja, se ele é igual a '+', '-', '*', '/' ou
 Caso contrário, "false".
 - O desafio é fazer o retorno sem usar "if" ou "switch".
 */
-// ?
+function isOperatorValid(operator) {
+  return !!operation[operator];
+}
 
 /*
 Agora vamos criar a calculadora.
@@ -50,7 +69,15 @@ parâmetros;
 operador passado para a função "calculator", e passando para esse método
 os dois parâmetros da função de retorno de "calculator".
 */
-// ?
+function calculator(operator) {
+  if (isOperatorValid(operator)) {
+    return function(number1, number2) {
+      return operation[operator](number1, number2);
+    }
+  }
+
+  return false;
+}
 
 /*
 Crie uma função chamada "showOperationMessage" que recebe três parâmetros:
@@ -59,7 +86,9 @@ deve ser a frase:
 'A operação [NUMBER1] [OPERATOR] [NUMBER2] =';
 Essa função mostrará a mensagem da operação que criaremos mais abaixo.
 */
-// ?
+function showOperationMessage(operator, number1, number2) {
+  return 'A operação ' + number1 + ' ' + operator + ' ' + number2 + ' = ';
+}
 
 /*
 Crie uma função chamada "showErrorMessage" que recebe um parâmetro: o
@@ -67,7 +96,9 @@ operador da operação cálculo, quando a operação não for válida.
 Essa função deverá retornar a frase:
 'Operação "[OPERATOR]" não permitida!'
 */
-// ?
+function showErrorMessage(operator) {
+  return 'Operação ' + operator + ' não permitida!';
+}
 
 /*
 Nossa calculadora está pronta! Agora vamos testá-la:
@@ -75,7 +106,9 @@ PASSO 1:
 - Declare 3 variáveis: "number1" e "number2", iniciando com valor zero, e
 "operationSignal", sem valor por enquanto.
 */
-// ?
+var number1 = 100;
+var number2 = 10;
+var operationSignal;
 
 /*
 PASSO 2:
@@ -83,7 +116,8 @@ Atribua à variável operationSignal o operador de soma, e declare uma
 variável chamada "sum", que receba a função "calculator", passando por
 parâmetro a variável que recebeu o sinal da operação.
 */
-// ?
+operationSignal = '+';
+var sum = calculator(operationSignal);
 
 /*
 PASSO 3:
@@ -97,17 +131,42 @@ parâmetros para o método "log" de "console":
 - O segundo, a função de soma, passando os dois operandos.
 - Se "sum" for "false", mostrar no console a mensagem de erro.
 */
-// ?
+console.log(showOperationMessage(operationSignal, number1, number2));
+console.log(sum(number1, number2));
 
 /*
 Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
 divisão e resto. Crie variáveis com os nomes "subtraction",
 "multiplication", "division" e "mod".
 */
-// ?
+operationSignal = '-';
+var subtraction = calculator(operationSignal);
+console.log(showOperationMessage(operationSignal, number1, number2));
+console.log(subtraction(number1, number2));
+
+operationSignal = '*';
+var multiplication = calculator(operationSignal);
+console.log(showOperationMessage(operationSignal, number1, number2));
+console.log(multiplication(number1, number2));
+
+operationSignal = '/';
+var division = calculator(operationSignal);
+
+console.log(showOperationMessage(operationSignal, number1, number2));
+console.log(division(number1, number2));
+
+operationSignal = '%';
+var mod = calculator(operationSignal);
+
+console.log(showOperationMessage(operationSignal, number1, number2));
+console.log(mod(number1, number2));
 
 /*
 Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
 a mensagem de erro será mostrada no console.
 */
-// ?
+operationSignal = '@';
+var invalidOperation = calculator(operationSignal);
+console.log(showErrorMessage(operationSignal));
+console.log(invalidOperation(number1, number2));
+})();
